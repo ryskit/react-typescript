4e0218b01468eea0ceec3190d3080bc96f0ad4a9
ryskit <ryosukekida.71@gmail.com>
2022-10-23 16:12:35 +0900

    :sparkles: 型定義を別ファイルで管理

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,30 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
-import { Todo } from './Todo';
-
-type TodoType = {
-  id: number;
-  userId: number;
-  title: string;
-  completed: boolean;
-};
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { Todo } from "./Todo";
+import { TodoType } from "./types/todo";
 
 function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
 
   const onClickFetchData = () => {
-    axios.get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      setTodos(res.data);
-    })
-  }
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
+  };
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
-        <Todo title={todo.title} userId={todo.userId} completed={todo.completed} />
+        <Todo
+          key={todo.id}
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
       ))}
     </div>
   );
--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,12 +1,7 @@
-type TodoType = {
-  title: string;
-  userId: number;
-  completed?: boolean;
-};
-export const Todo = (props: TodoType) => {
+import { TodoType } from "./types/todo";
+
+export const Todo = (props: Omit<TodoType, "id">) => {
   const { title, userId, completed = false } = props;
   const completeMark = completed ? "[完]" : "[未]";
-  return (
-    <p>{`${completeMark} ${title}ユーザー:${userId}`}</p>
-  );
-}
+  return <p>{`${completeMark} ${title}ユーザー:${userId}`}</p>;
+};
